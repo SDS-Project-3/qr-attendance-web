e08117e59fa269461d7fb6050c8bc6f6044f0a32
Afwan-Rezal <21b6027@ubd.edu.bn>
2024-09-18 22:20:16 +0800

    Added Session table

--- database/migrations/2024_09_13_151212_create_users_table.php
+++ database/migrations/2024_09_13_151212_create_users_table.php
@@ -27,6 +27,15 @@ return new class extends Migration
             $table->timestamps();
         });
 
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+
     }
 
     /**
@@ -35,5 +44,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('students');
+        Schema::dropIfExists('lecturers');
+        Schema::dropIfExists('sessions');
     }
 };
